6f955d02bf7942916663344a79a592eab90e79ec
Shumbusho David <75299674+Shumbusho43@users.noreply.github.com>
2022-02-12 21:57:06 +0200

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,7 +7,6 @@ const {
   assignMatch,
   gettingResult,
   addToken,
-  deleteUser,
 } = require("../controllers/user.controller");
 const { protect } = require("../middleware/auth");
 const router = express.Router();
@@ -17,6 +16,4 @@ router.route("/getMatch").get(protect, gettingResult);
 router.route("/interest").post(protect, registerInterests);
 router.route("/addToken").post(addToken);
 router.route("/:id").get(getUser);
-router.route("/delete/:id").delete(deleteUser)
-
 module.exports.userRoutes = router;
